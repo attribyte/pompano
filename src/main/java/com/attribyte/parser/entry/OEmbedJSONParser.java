a3c6e583ee1ae043a6888c58e7e8f4833e63dd8a
Matt Hamer <matt@attribyte.com>
2018-04-27 11:37:20 -0500

    Initial add of oembed parser/provider + provider list parsing.

---
+++ src/main/java/com/attribyte/parser/entry/OEmbedJSONParser.java
@@ -0,0 +1,150 @@
+/*
+ * Copyright 2018 Attribyte, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ *
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied.
+ *
+ * See the License for the specific language governing permissions
+ * and limitations under the License.
+ */
+
+package com.attribyte.parser.entry;
+
+import com.attribyte.parser.ContentCleaner;
+import com.attribyte.parser.DefaultAMPCleaner;
+import com.attribyte.parser.ParseError;
+import com.attribyte.parser.ParseResult;
+import com.attribyte.parser.model.Author;
+import com.attribyte.parser.model.Entry;
+import com.attribyte.parser.model.Image;
+import com.attribyte.parser.model.Resource;
+import com.attribyte.parser.model.Video;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectReader;
+import com.google.common.base.Strings;
+import com.google.common.collect.ImmutableList;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.Iterator;
+
+import static com.attribyte.parser.DateParser.tryParseISO8601;
+import static com.attribyte.parser.Util.*;
+
+/**
+ * Parse HTML metadata to create an entry.
+ * @author Matt Hamer.
+ */
+public class OEmbedJSONParser implements com.attribyte.parser.Parser {
+
+   @Override
+   public String name() {
+      return "oembed-json";
+   }
+
+   @Override
+   public ParseResult parse(final String content, final String sourceLink, final ContentCleaner contentCleaner) {
+
+      try {
+         Resource.Builder resource = Resource.builder();
+         Entry.Builder entry = Entry.builder();
+         resource.addEntry(entry.build());
+
+         resource.setSourceLink(sourceLink);
+         JsonNode root = jsonReader.readTree(content);
+
+         String title = root.path("title").asText();
+         if(!title.isEmpty()) {
+            entry.setTitle(title);
+         }
+
+         String authorName = root.path("author_name").asText();
+         String authorURL = root.path("author_url").asText();
+         if(!authorName.isEmpty()) {
+            Author.Builder author = Author.builder(authorName);
+            if(!authorURL.isEmpty()) {
+               author.setLink(authorURL);
+            }
+            entry.addAuthor(author.build());
+         }
+
+         String thumbnailURL = root.path("thumbnail_url").asText();
+         if(!thumbnailURL.isEmpty()) {
+            Image.Builder primaryImage = Image.builder(thumbnailURL);
+            int thumbnailWidth = root.path("thumbnail_width").asInt();
+            if(thumbnailWidth > 0) {
+               primaryImage.setWidth(thumbnailWidth);
+            }
+            int thumbnailHeight = root.path("thumbnail_height").asInt();
+            if(thumbnailHeight > 0) {
+               primaryImage.setHeight(thumbnailHeight);
+            }
+            entry.setPrimaryImage(primaryImage.build());
+         }
+
+         String type = root.path("type").asText();
+         switch(type) {
+            case "photo": {
+               String url = root.path("url").asText();
+               if(!url.isEmpty()) {
+                  Image.Builder image = Image.builder(url);
+                  int width = root.path("width").asInt();
+                  if(width > 0) {
+                     image.setWidth(width);
+                  }
+                  int height = root.path("height").asInt();
+                  if(height > 0) {
+                     image.setHeight(height);
+                  }
+                  if(entry.getPrimaryImage() == null) {
+                     entry.setPrimaryImage(image.build());
+                  } else {
+                     entry.addImage(image.build());
+                  }
+               }
+            }
+            break;
+            case "video":
+               String videoContent = root.path("html").asText();
+               if(!videoContent.isEmpty()) {
+                  Document doc = Jsoup.parse(videoContent, sourceLink, org.jsoup.parser.Parser.htmlParser());
+                  entry.setOriginalContent(doc);
+                  entry.setCleanContent(contentCleaner.toCleanContent(doc));
+               }
+               break;
+            case "rich":
+               String richContent = root.path("html").asText();
+               if(!richContent.isEmpty()) {
+                  Document doc = Jsoup.parse(richContent, sourceLink, org.jsoup.parser.Parser.htmlParser());
+                  entry.setOriginalContent(doc);
+                  entry.setCleanContent(contentCleaner.toCleanContent(doc));
+               }
+               break;
+         }
+
+         return new ParseResult(name(), resource.build());
+
+      } catch(Error e) {
+         throw e;
+      } catch(Throwable t) {
+         return new ParseResult(name(), new ParseError("HTML AMP Parser Failure", t));
+      }
+   }
+
+   /**
+    * The JSON reader.
+    */
+   private final ObjectReader jsonReader = new ObjectMapper().reader();
+}
---
+++ src/main/java/com/attribyte/parser/model/OEmbedProvider.java
@@ -0,0 +1,180 @@
+/*
+ * Copyright 2018 Attribyte, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ *
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied.
+ *
+ * See the License for the specific language governing permissions
+ * and limitations under the License.
+ */
+
+package com.attribyte.parser.model;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.base.MoreObjects;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Lists;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * An immutable oembed provider.
+ */
+public class OEmbedProvider {
+
+   public static class Endpoint {
+
+      /**
+       * Creates an endpoint.
+       * @param url The endpoint URL.
+       * @param schemes The collection of schemes.
+       */
+      public Endpoint(final String url, final Collection<String> schemes) {
+         this.schemes = schemes != null ? ImmutableList.copyOf(schemes) : ImmutableList.of();
+         this.url = url;
+      }
+
+      @Override
+      public String toString() {
+         return MoreObjects.toStringHelper(this)
+                 .add("schemes", schemes)
+                 .add("url", url)
+                 .toString();
+      }
+
+      /**
+       * The immutable list of schemes.
+       */
+      public final ImmutableList<String> schemes;
+
+      /**
+       * The endpoint URL.
+       */
+      public final String url;
+   }
+
+   /**
+    * Creates an oembed provider.
+    * @param name The provider name.
+    * @param url The provider URL.
+    * @param endpoints The collection of endpoints.
+    */
+   public OEmbedProvider(final String name, final String url, final Collection<Endpoint> endpoints) {
+      this.name = name;
+      this.url = url;
+      this.endpoints = endpoints != null ? ImmutableList.copyOf(endpoints) : ImmutableList.of();
+   }
+
+   @Override
+   public String toString() {
+      return MoreObjects.toStringHelper(this)
+              .add("name", name)
+              .add("url", url)
+              .add("endpoints", endpoints)
+              .toString();
+   }
+
+   /**
+    * The provider name.
+    */
+   public final String name;
+
+   /**
+    * The provider URL.
+    */
+   public final String url;
+
+   /**
+    * The immutable list of endpoints.
+    */
+   public final ImmutableList<Endpoint> endpoints;
+
+   /**
+    * Creates a map containing the default published providers.
+    * @return The map of providers vs URL.
+    * @throws IOException on parse/load error.
+    */
+   public static ImmutableMap<String, OEmbedProvider> publishedProviders() throws IOException {
+      return publishedProviders(DEFAULT_PROVIDERS_URL);
+   }
+
+
+   /**
+    * Creates a map containing published providers from a URL.
+    * @return The map of providers vs URL.
+    * @throws IOException on parse/load error.
+    */
+   public static ImmutableMap<String, OEmbedProvider> publishedProviders(final String url) throws IOException {
+      InputStream is = new URL(url).openStream();
+      try {
+         return fromJSON(new ObjectMapper().readTree(is));
+      } catch(MalformedURLException mue) {
+         throw new IOException(String.format("Invalid URL, '%s'", url), mue);
+      } finally {
+         if(is != null) {
+            is.close();
+         }
+      }
+   }
+
+   /**
+    * Creates a map of provider vs URL from the format supported by {@code https://oembed.com/providers.json}.
+    * @param node The root node.
+    * @return The map of provider vs URL.
+    */
+   public static ImmutableMap<String, OEmbedProvider> fromJSON(final JsonNode node) {
+      if(node.isArray()) {
+         ImmutableMap.Builder<String, OEmbedProvider> providerBuilder = ImmutableMap.builder();
+         for(final JsonNode providerNode : node) {
+            String providerName = providerNode.path("provider_name").asText();
+            String providerURL = providerNode.path("provider_url").asText();
+            if(!providerURL.isEmpty()) {
+               JsonNode endpointsNode = providerNode.path("endpoints");
+               if(endpointsNode.isArray()) {
+                  List<Endpoint> endpoints = Lists.newArrayListWithExpectedSize(1024);
+                  for(JsonNode endpointNode : endpointsNode) {
+                     String endpointURL = endpointNode.path("url").asText();
+                     if(!endpointURL.isEmpty()) {
+                        JsonNode schemesNode = endpointNode.path("schemes");
+                        if(schemesNode.isArray()) {
+                           List<String> schemes = Lists.newArrayListWithExpectedSize(4);
+                           for(JsonNode schemeNode : schemesNode) {
+                              String scheme = schemeNode.asText();
+                              if(!scheme.isEmpty()) {
+                                 schemes.add(scheme);
+                              }
+                           }
+                           endpoints.add(new Endpoint(endpointURL, schemes));
+                        }
+                     }
+                  }
+                  providerBuilder.put(providerURL, new OEmbedProvider(providerName, providerURL, endpoints));
+               }
+            }
+         }
+         return providerBuilder.build();
+      } else {
+         return ImmutableMap.of();
+      }
+   }
+
+   /**
+    * The standard oembed providers endpoint URL ({@value}).
+    */
+   public static final String DEFAULT_PROVIDERS_URL = "https://oembed.com/providers.json";
+}
