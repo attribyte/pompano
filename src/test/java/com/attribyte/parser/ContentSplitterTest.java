53a53657347811cc9c8b4405ed8b80e24e4a4db4
matt <matt@attribyte.com>
2020-12-07 14:46:40 -0500

    Add more tests

--- src/test/java/com/attribyte/parser/ContentSplitterTest.java
+++ src/test/java/com/attribyte/parser/ContentSplitterTest.java
@@ -18,9 +18,7 @@
 
 package com.attribyte.parser;
 
-import com.google.common.base.Charsets;
 import com.google.common.collect.ImmutableSet;
-import com.google.common.collect.Maps;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -31,8 +29,6 @@ import org.junit.Test;
 
 import java.io.File;
 import java.util.List;
-import java.util.Map;
-import java.util.function.Function;
 
 import static org.junit.Assert.*;
 
@@ -79,7 +75,7 @@ public class ContentSplitterTest extends ResourceTest {
 
    @Test
    public void translate_inline_with_break() throws Exception {
-      String content = "Leading text <h1>The title</h1> Trailing text";
+      String content = "Leading text <h1>The <b>title</b>.</h1> Trailing text";
       Document doc = Jsoup.parseBodyFragment(content, "");
       ContentSplitter contentSplitter = new ContentSplitter("p",
               Whitelists.blockElementNames,
@@ -91,13 +87,51 @@ public class ContentSplitterTest extends ResourceTest {
 
 
       Elements splitElements = contentSplitter.split(doc.body());
+      System.out.println(splitElements.toString());
       assertEquals(2, splitElements.size());
       assertEquals("Leading text", splitElements.get(0).text());
       List<Node> children = splitElements.get(1).childNodes();
       assertEquals(3, children.size());
-      assertElementText(children.get(0), "b", "The title");
+      assertElementText(children.get(0), "b", "The title.");
       assertElementText(children.get(1), "br", "");
       assertNodeText(children.get(2), "Trailing text");
+   }
+
+
+   @Test
+   public void translate_nested() throws Exception {
+      String content = "Leading text <div><p><span>Trailing <b>text</b></span></p></div>";
+      Document doc = Jsoup.parseBodyFragment(content, "");
+      ContentSplitter contentSplitter = new ContentSplitter("p",
+              ImmutableSet.of("div", "span", "p"),
+              ImmutableSet.copyOf(new String[] {
+                      "ul",
+                      "img",
+                      "figure"
+              }), null);
+
+
+      Elements splitElements = contentSplitter.split(doc.body());
+      System.out.println(splitElements.toString());
+      assertEquals(2, splitElements.size());
+      assertEquals("Leading text", splitElements.get(0).text());
+      assertEquals("Trailing <b>text</b>", splitElements.get(1).html());
+   }
+
+   @Test
+   public void image_in_anchor() throws Exception {
+      String content = "<div><a href='https://attribyte.com'><img src='https://attribyte.com/img1.jpg'/></a></div>";
+      Document doc = Jsoup.parseBodyFragment(content, "");
+      ContentSplitter contentSplitter = new ContentSplitter("p",
+              Whitelists.blockElementNames,
+              ImmutableSet.of("img")
+              , null);
+      Elements splitElements = contentSplitter.split(doc.body());
+      assertEquals(1, splitElements.size());
+      assertEquals(1, splitElements.get(0).children().size());
+      assertEquals("a", splitElements.get(0).child(0).tagName());
+      assertEquals(1, splitElements.get(0).child(0).children().size());
+      assertEquals("img", splitElements.get(0).child(0).child(0).tagName());
    }
 
    private void assertNodeText(final Node node, final String value) {
